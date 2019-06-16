4c4e725f018d3df015e50163acc97d04a6f40f40
Eric Lowry <eric@myrealms.org>
2019-06-16 20:58:56 +0200

    Added browser, os and version checking.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import { IotpProvider } from "./state/useIotp";
-import { ThingyProvider } from "./state/useThingy";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { IotpProvider } from './state/useIotp';
+import { ThingyProvider } from './state/useThingy';
 
-import "./index.scss";
+import { detect } from 'detect-browser';
+
+import './index.scss';
+
+const { name, version, os } = detect();
 
 ReactDOM.render(
-  <IotpProvider>
-    <ThingyProvider>
-      <App />
-    </ThingyProvider>
-  </IotpProvider>,
-  document.getElementById("root")
+    name === 'chrome' && os === 'Mac OS' ? (
+        <IotpProvider>
+            <ThingyProvider>
+                <App />
+            </ThingyProvider>
+        </IotpProvider>
+    ) : (
+        <div style={{marginTop: 50, marginLeft: 50 }}>
+            Sorry, "{name}" on "{os}" is an unsupported browser.
+            <br />
+            <br />
+            <br />
+            Please use a recent version of{' '}
+            <a href="https://www.google.com/chrome/">Chrome</a>{' '} on MacOS.
+        </div>
+    ),
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
